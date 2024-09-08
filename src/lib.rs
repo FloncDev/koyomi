b7c5d3807342745600cd65a6a9f3cd19e6a4a52b
FloncDev <FloncDev@users.noreply.github.com>
2024-09-08 22:15:11 +0100

    :construction: Axum boilerplate

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+use reqwest::Client;
+use sqlx::{Pool, Postgres};
+
+#[derive(Clone)]
+pub struct AppState {
+    pub pool: Pool<Postgres>,
+    pub scraper: Client,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,46 @@
-use std::collections::HashMap;
-
-use chrono::{Duration, NaiveDate, NaiveDateTime, NaiveTime, Weekday};
-use icalendar::{Calendar, Component, Event, EventLike};
-use regex::Regex;
+use axum::{routing::get, Router};
+use dotenvy::var;
+use koyomi::AppState;
 use reqwest::{header::HeaderMap, Client};
-use scraper::{selectable::Selectable, Html, Selector};
-use uuid::Uuid;
-
-#[derive(Debug)]
-struct Lesson {
-    name: String,
-    teachers: String,
-    location: String,
-    start: NaiveDateTime,
-    end: NaiveDateTime,
-    uid: Uuid,
-}
-
-impl From<Lesson> for Event {
-    fn from(value: Lesson) -> Self {
-        Event::new()
-            .location(&value.location)
-            .summary(&value.name)
-            .description(&value.teachers)
-            .starts(value.start)
-            .ends(value.end)
-            .done()
-    }
-}
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt().init();
+    dotenvy::dotenv().expect("Could not find .env file");
+
+    // Setup state
+    let pool = PgPoolOptions::new()
+        .connect(
+            var("DATABASE_URL")
+                .expect("DATABASE_URL is not set")
+                .as_str(),
+        )
+        .await
+        .expect("Error connecting to database");
 
     let mut headers = HeaderMap::new();
-    headers.append("cookie", "redacted".parse().unwrap());
+    headers.append(
+        "cookie",
+        var("COOKIE").expect("COOKIE is not set").parse().unwrap(),
+    );
     headers.append("x-requested-with", "XMLHttpRequest".parse().unwrap());
 
-    let client = Client::builder().default_headers(headers).build()?;
+    let scraper = Client::builder().default_headers(headers).build()?;
 
-    let mut form = HashMap::new();
+    let state = AppState { pool, scraper };
 
-    let week_starting = "2024-09-09";
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .with_state(state);
 
-    form.insert("week", week_starting);
-    form.insert("student_user_id", "redacted");
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .expect("Cannot bind to port 3000");
 
-    let html = client
-        .post("redacted")
-        .form(&form)
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    let html = Html::parse_fragment(&html);
-
-    let well_selector = Selector::parse(".well")?;
-    let h3_selector = Selector::parse("h3")?;
-    let lesson_selector = Selector::parse("li")?;
-
-    let regex =
-        Regex::new(r"<strong>(.*?) - (.*?):<\/strong> (.*?) in (.*?) with  (.*?$)").unwrap();
-
-    let week_start = NaiveDate::parse_from_str(week_starting, "%Y-%m-%d").unwrap();
-
-    let mut lessons = vec![];
-
-    for element in html.select(&well_selector) {
-        let weekday = element
-            .select(&h3_selector)
-            .next()
-            .unwrap()
-            .inner_html()
-            .parse::<Weekday>()?;
-
-        for lesson in element.select(&lesson_selector) {
-            let inner_html = lesson.inner_html();
-            let captures = regex
-                .captures(inner_html.trim())
-                .unwrap()
-                .iter()
-                .map(|capture| capture.unwrap().as_str().to_string())
-                .collect::<Vec<String>>();
-
-            if let [_, start_time, end_time, name, location, teachers] = &captures[..] {
-                let start_time = NaiveTime::parse_from_str(start_time, "%H:%M").unwrap();
-                let end_time = NaiveTime::parse_from_str(end_time, "%H:%M").unwrap();
-
-                let start = NaiveDateTime::new(
-                    week_start + Duration::days(weekday.num_days_from_monday().into()),
-                    start_time,
-                );
-
-                let end = NaiveDateTime::new(
-                    week_start + Duration::days(weekday.num_days_from_monday().into()),
-                    end_time,
-                );
-
-                lessons.push(Lesson {
-                    name: name.to_owned(),
-                    teachers: teachers.to_owned(),
-                    location: location.to_owned(),
-                    start,
-                    end,
-                    uid: Uuid::new_v4(),
-                });
-            } else {
-                panic!();
-            };
-        }
-    }
-
-    let mut cal = Calendar::new();
-
-    for lesson in lessons {
-        cal.push(std::convert::Into::<Event>::into(lesson));
-    }
-
-    std::fs::write("cal.ical", cal.to_string()).unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 
     Ok(())
 }
