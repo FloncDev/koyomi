082c8bf7f044f7d7824c11e581dd1aa42a2277b8
FloncDev <FloncDev@users.noreply.github.com>
2024-11-01 11:28:49 +0000

    ✨ Create genereate calendar function

--- koyomi-api/src/routes/ical.rs
+++ koyomi-api/src/routes/ical.rs
@@ -1,7 +1,31 @@
-use axum::Router;
+use axum::{response::IntoResponse, routing::get, Router};
+use icalendar::{Calendar, Component, Event, EventLike};
+use koyomi_core::Lesson;
 
 use crate::AppState;
 
+// Seperate function so I can do testing
+pub fn generate_calendar(lessons: Vec<Lesson>) -> Calendar {
+    let mut calendar = Calendar::new();
+
+    for lesson in lessons {
+        calendar.push(
+            Event::new()
+                .summary(&lesson.subject)
+                .description(&lesson.teachers)
+                .location(&lesson.location)
+                .starts(lesson.start.to_utc())
+                .ends(lesson.end.to_utc())
+                .uid(format!("{}-{}-{}", lesson.subject, lesson.start, lesson.end).as_str())
+                .done(),
+        );
+    }
+
+    calendar
+}
+
+async fn get_ical() -> impl IntoResponse {}
+
 pub fn get_router() -> Router<AppState> {
-    Router::new()
+    Router::new().route("/calendar.ical", get(get_ical))
 }
